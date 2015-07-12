7ab4d5203c10fe5642bbe9780fec80e7c835b981
fdophin <fdophin@gmail.com>
2015-07-13 02:51:13 +0300

    blabla

--- dz2_1/dz07.java
+++ dz2_1/dz07.java
@@ -2,11 +2,39 @@
 (например, 1234321 – палиндром, 12345 – не палиндром).*/
 package dz2_1;
 
+import java.util.Scanner;
+
 public class dz07 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/* пример
+		Scanner sn = new Scanner(System.in);
+		System.out.print("Введите целое число: ");
 
+		long a = sn.nextLong();
+
+		byte count, i;
+
+		boolean palindrom = true;
+
+		for ( count=1; (a/(int)Math.pow(10,count))!=0; count++ );
+
+		for ( i=0; i<count; i++ )
+
+		palindrom &= ( (a/(int)Math.pow(10,i)) %10 == a/(int)Math.pow(10,count-1-i) %10 )?true:false; 
+
+		System.out.printf("Введенное число %s полиндромом", (palindrom)?"является":"не является");*/
+
+			System.out.print("Введи число: ");
+	      	Scanner scan = new Scanner(System.in);
+			String n = scan.nextLine();
+			String Re = new StringBuffer(n).reverse().toString();
+			System.out.println((n.equals (Re))? "Полиндром": "Неполиндром" );
+		     
+	
+		
+		
+	
 	}
 
 }
