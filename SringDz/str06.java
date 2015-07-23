0be42bf1075ddac6359723906971f91325cb7c26
fdophin <fdophin@gmail.com>
2015-07-24 00:33:41 +0300

    bla

--- SringDz/str06.java
+++ SringDz/str06.java
@@ -13,7 +13,7 @@ public class str06 {
 		int i = 0, pos = W_Blake.indexOf(word); //Тут указана первая позиция слова
 		     while (pos  >= 0) {  //перебор до тех пор пока pos не станет -1
 	            
-	        	pos  = W_Blake.indexOf(word, pos + 1  ); 
+	        	pos  = W_Blake.indexOf(word, pos + 1  ); // 2 значю после запятой указывает на позицию отсчета
 	           i++;
 	           System.out.println(pos);
 	        }
